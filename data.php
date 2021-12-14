4be9fdb062732e234e425a5d40c6f19455b6c183
IHaveBoughtWinRar <ivebotwinrar@mail.ru>
2021-12-14 15:20:55 +0100

    complete first task. lot.html renamed to lot.php and php script added to make this page work with data.php

--- data.php
+++ data.php
@@ -6,3 +6,38 @@ $bets = [
     ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
     ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
 ];
+
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+$items = [];
+array_push($items, ["id" => 1,
+    "title" => '2014 Rossignol District Snowboard',
+    "category" => $categories[0],
+    "price" => 10999,
+    "URL" => "img/lot-1.jpg"]);
+array_push($items, ["id" => 2,
+    "title" => 'DC Ply Mens 2016/2017 Snowboard',
+    "category" => $categories[0],
+    "price" => 15999,
+    "URL" => "img/lot-2.jpg"]);
+array_push($items, ["id" => 3,
+    "title" => 'Крепления Union Contact Рго 2015 года размер L/XL',
+    "category" => $categories[1],
+    "price" => 8000,
+    "URL" => "img/lot-3.jpg"]);
+array_push($items, ["id" => 4,
+    "title" => 'Ботинки для сноуборда DC Mutiny Charocal',
+    "category" => $categories[2],
+    "price" => 10999,
+    "URL" => "img/lot-4.jpg"]);
+array_push($items, ["id" => 5,
+    "title" => 'Куртка для сноуборда ОС Mutiny Charocal',
+    "category" => $categories[3],
+    "price" => 7500,
+    "URL" => "img/lot-5.jpg"]);
+array_push($items, ["id" => 6,
+    "title" => 'Маска Oakley Сапору',
+    "category" => $categories[5],
+    "price" => 5400,
+    "URL" => "img/lot-6.jpg"]);
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+require_once "util.php";
+require_once("data.php");
+
 $is_auth = (bool) rand(0, 1);
+
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -64,17 +70,18 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php foreach ($items as $item) :?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$item['URL']?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$item['category']?>></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.php?id=<?=$item['id']?>"><?=$item['title']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=format_price($item['price'])?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -82,6 +89,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+            <?php endforeach;?>
         </ul>
     </section>
 </main>
