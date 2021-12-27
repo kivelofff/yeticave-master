8ed73b8190f56b4b52662eca455a860bc20bb625
laborant <stupidl@mail.com>
2021-12-27 02:07:53 +0100

    tc2

--- add.php
+++ add.php
@@ -1,1 +1,13 @@
 <?php
+require_once 'functions.php';
+$method =$_SERVER['REQUEST_METHOD'];
+
+if ($method == 'GET') {
+    $layout_content = renderTemplate('templates/layout.php',['title'=>'Add lot', 'content'=>file_get_contents('templates/add-lot.php')]);
+    print($layout_content);
+
+} elseif ($method == 'POST') {
+
+} else {
+    http_response_code(400);
+}
